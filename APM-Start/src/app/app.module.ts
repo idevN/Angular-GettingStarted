7467b97422567f497ab245fc5980e2e60e2cbce4
nigeljoy <devnigeljoy@gmail.com>
2017-10-11 17:18:00 +0530

    upto section6
    understanding more about ng2

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
-
+import {ProductListComponent} from './products/product-list.component';
+import {ProductFilterPipe} from './products/product-filter-pipe'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
